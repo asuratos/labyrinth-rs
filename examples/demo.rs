c978c73820835b5e5e8d2d6f8e0546d5ec0d739d
Aldrich Suratos <cyurio@gmail.com>
2021-09-21 11:51:10 +0900

    more groundwork

---
+++ examples/demo.rs
@@ -0,0 +1,23 @@
+use bracket_terminal::prelude::*;
+use labyrinth_rs;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.print(
+            1,
+            1,
+            "TODO: running terminal for continuous generation of maps",
+        );
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Hello Minimal Bracket World")
+        .build()?;
+
+    let gs: State = State {};
+    main_loop(context, gs)
+}
--- src/bin/bin.rs
+++
@@ -1,5 +0,0 @@
-use labyrinth_rs;
-
-fn main() {
-
-}
