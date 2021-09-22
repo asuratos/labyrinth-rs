cf5b3df93b9253836845d400e12dfd8dd24ac1b4
Aldrich Suratos <cyurio@gmail.com>
2021-09-22 18:23:25 +0900

    implemented bracket-pathfinding traits on map struct (really only for Dijkstra maps and checking connectivity)

--- src/map_generators.rs
+++ src/map_generators.rs
@@ -2,29 +2,98 @@
 
 use std::collections::HashMap;
 
-use bracket_geometry::prelude::Point;
+use bracket_geometry::prelude::*;
+use bracket_pathfinding::prelude::*;
 
 use super::errors::BuilderError;
 
-use map_objects::{FinishedMap, TileType};
+use map_objects::Map;
 
 mod map_objects {
+    use super::{Algorithm2D, BaseMap, DistanceAlg, Point, SmallVec};
     use serde::{Deserialize, Serialize};
-    #[derive(Clone, Copy, Serialize, Deserialize)]
-    pub enum TileType {
+
+    #[derive(Clone, Copy, PartialEq, Serialize, Deserialize)]
+    pub enum TileKind {
         Floor,
         Wall,
         Door,
     }
 
-    /// Output struct for
-    #[derive(Serialize, Deserialize)]
-    pub struct FinishedMap {
-        pub map: Vec<TileType>,
-        pub width: usize,
-        pub height: usize,
-        // pub entities: Option<HashMap<String, Point>>,
-        // TODO: allow the builder to populate the map
+    #[derive(Clone, Copy, PartialEq, Serialize, Deserialize)]
+    pub struct Tile {
+        // kind: TileKind,
+        opaque: bool,
+        enterable: bool,
+        // flyable: bool
+    }
+
+    impl Default for Tile {
+        fn default() -> Self {
+            Tile {
+                // kind: TileKind::Wall,
+                opaque: true,
+                enterable: false,
+            }
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Clone)]
+    pub struct Map {
+        pub tiles: Vec<Tile>,
+        pub dimensions: Point,
+    }
+
+    // Implementing Algorithm2D from bracket-pathfinding on map
+    // This gives access to some useful helper methods using bracket-lib Points
+    impl Algorithm2D for Map {
+        fn dimensions(&self) -> Point {
+            self.dimensions
+        }
+    }
+
+    impl BaseMap for Map {
+        fn get_available_exits(&self, _idx: usize) -> SmallVec<[(usize, f32); 10]> {
+            let mut neighbors = SmallVec::new();
+
+            let start = self.index_to_point2d(_idx);
+            let deltas = &[
+                Point::new(-1, 0),
+                Point::new(0, -1),
+                Point::new(1, 0),
+                Point::new(0, 1),
+            ];
+
+            for diff in deltas {
+                let neighbor = self.point2d_to_index(start + *diff);
+                if self.tiles[neighbor].enterable {
+                    neighbors.push((neighbor, 1.0));
+                }
+            }
+
+            neighbors
+        }
+
+        fn get_pathing_distance(&self, _idx1: usize, _idx2: usize) -> f32 {
+            DistanceAlg::Pythagoras
+                .distance2d(self.index_to_point2d(_idx1), self.index_to_point2d(_idx2))
+        }
+    }
+
+    impl Map {
+        pub fn new(width: usize, height: usize) -> Map {
+            Map {
+                tiles: vec![Default::default(); width * height],
+                dimensions: Point::new(width, height),
+            }
+        }
+
+        pub fn new_from_dims(dimensions: Point) -> Map {
+            Map {
+                tiles: vec![Default::default(); (dimensions.x * dimensions.y) as usize],
+                dimensions: dimensions,
+            }
+        }
     }
 }
 
@@ -39,27 +108,27 @@ pub enum FloorGenAlg {
 /// ```rust
 /// use labyrinth_rs::prelude::*;
 ///
-/// let mut mapgen = MapGenerator2D::new(10,10);
-/// let map = mapgen.generate(FloorGenAlg::Basic);
+/// let mut mapgen = MapGenerator2D::new(80,50);
+/// let floor1 = mapgen.generate(FloorGenAlg::Basic);
+/// let floor2 = mapgen.generate(FloorGenAlg::Basic);
+/// let floor3 = mapgen.generate(FloorGenAlg::Basic);
 /// ```
 pub struct MapGenerator2D {
-    map: Vec<TileType>,
-    width: usize,
-    height: usize,
+    map: Map,
+    dimensions: Point,
 }
 
 impl MapGenerator2D {
     /// Creates a new Generator struct using width and height inputs
     pub fn new(width: usize, height: usize) -> MapGenerator2D {
         MapGenerator2D {
-            map: vec![TileType::Floor; width * height],
-            width: width,
-            height: height,
+            map: Map::new(width, height),
+            dimensions: Point::new(width, height),
         }
     }
 
     /// Generates a FinishedMap using the current settings.
-    pub fn generate(&mut self, method: FloorGenAlg) -> Result<FinishedMap, BuilderError> {
+    pub fn generate(&mut self, method: FloorGenAlg) -> Result<Map, BuilderError> {
         // Start with a new map
         self.flush_map();
 
@@ -77,15 +146,11 @@ impl MapGenerator2D {
             }
         };
 
-        Ok(FinishedMap {
-            map: self.map.to_owned(),
-            width: self.width,
-            height: self.height,
-        })
+        Ok(self.map.clone())
     }
 
     fn flush_map(&mut self) {
-        self.map = vec![TileType::Floor; self.width * self.height];
+        self.map = Map::new_from_dims(self.dimensions);
     }
 }
 
